f0d79017798abfd8f87148cb53a293a974ce2348
B.G.Adrian <btools@gmail.com>
2014-12-31 15:37:53 +0200

    more tests and fixes on affine and atbash

--- src/monoalphabetic.ciphers.js
+++ src/monoalphabetic.ciphers.js
@@ -124,30 +124,43 @@ var monoalphabetic = {
 		//Atbash with latin alphabet
 		'atbash': {
 			'from': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
-			'to': 'ZYXWVUTSRQPONMLKJIHGFEDCBA'.split('')
+			'to': 'ZYXWVUTSRQPONMLKJIHGFEDCBA'.split(''),
+			'checkLowercase': true
 		},
 		//keyword cipher with "KRYPTOS" as the keyword
 		'keywordEncrypt': {
 			'from': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
-			'to': 'KRYPTOSABCDEFGHIJLMNQUVWXZ'.split('')
+			'to': 'KRYPTOSABCDEFGHIJLMNQUVWXZ'.split(''),
+			'checkLowercase': true
 		},
 		'keywordDecrypt': {
 			'from': 'KRYPTOSABCDEFGHIJLMNQUVWXZ'.split(''),
-			'to': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+			'to': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+			'checkLowercase': true
 		}
 	},
 	customFunctionsOptions: {
+		//Affine a substitution cipher, but with a mathematical function
+		//http://en.wikipedia.org/wiki/Affine_cipher
 		affineEncrypt: {
+			//a or alfa
 			key: 5,//1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, or 25
 			keyInverse: 21,//modular multiplicative inverse of Key modulo Size
+			//b or beta
 			shiftMagnitudine: 8,
 			size: 26, //alphabet size, affineKey and affineSize must be coprime
 			alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
 			transform: function(latinCharacter, index, opts){
-				var x = (latinCharacter.toUpperCase().charCodeAt(0) - 65);
-				var asciiResult = ((opts.key * x) + opts.shiftMagnitudine) % opts.size;
+				if (opts.alphabet.indexOf(latinCharacter) == -1){
+					return latinCharacter;
+				}
 
-				return String.fromCharCode(asciiResult + 65)
+				var isLowercase = (latinCharacter == latinCharacter.toLowerCase());
+				var asciiOffset = (isLowercase ? 97: 65);
+				var x = (latinCharacter.charCodeAt(0) - asciiOffset);
+				//x must be 0-25 because size is 26
+				var asciiResult = ((opts.key * x) + opts.shiftMagnitudine) % opts.size;
+				return String.fromCharCode(asciiResult + asciiOffset);
 			}
 		},
 		affineDecrypt: {
@@ -157,12 +170,18 @@ var monoalphabetic = {
 			size: 26, //alphabet size, affineKey and affineSize must be coprime
 			alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
 			transform: function(latinCharacter, index, opts){
-				var y = (latinCharacter.toUpperCase().charCodeAt(0) - 65);
+				if (opts.alphabet.indexOf(latinCharacter) == -1){
+					return latinCharacter;
+				}
+
+				var isLowercase = (latinCharacter == latinCharacter.toLowerCase());
+				var asciiOffset = (isLowercase ? 97: 65);
+				var y = (latinCharacter.charCodeAt(0) - asciiOffset);
 				var tmp = (opts.keyInverse * (y - opts.shiftMagnitudine));
 				//because modulo in javascript doesn't play well with negative numbers
 				//http://stackoverflow.com/questions/4467539/javascript-modulo-not-behaving
 				var asciiResult = ((tmp % opts.size) + opts.size) % opts.size;
-				return String.fromCharCode(asciiResult + 65)
+				return String.fromCharCode(asciiResult + asciiOffset)
 			}
 		}
 	}
@@ -182,6 +201,7 @@ if (typeof(module) !== 'undefined'){
 //http://www.cs.utsa.edu/~wagner/laws/pad.html
 //can be used for Stream ciphers too
 //http://en.wikipedia.org/wiki/Stream_cipher
+//Supports keys smaller then the text too
 
 var oneTimeKey = 'XMCKL';
 var oneTimeAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
--- tests/monoalphabetic.tests.js
+++ tests/monoalphabetic.tests.js
@@ -37,18 +37,31 @@ test('Mono shift caesar', function(){
 	ok(mono.shift(mono.shift("007AaZz", mono.shiftOptions.caesarEncrypt), mono.shiftOptions.caesarDecrypt) == '007AaZz', 'encrypt/decrypt');
 });
 
+//checked with http://rumkin.com/tools/cipher/atbash.php
 test('Mono substitution AtBash', function(){
 	ok(mono.substitution("MONEY", mono.substitutionOptions.atbash) == 'NLMVB', 'Encrypt');
 	ok(mono.substitution("NLMVB", mono.substitutionOptions.atbash) == 'MONEY', 'Decrypt');
+	ok(mono.substitution("small caps", mono.substitutionOptions.atbash) == 'hnzoo xzkh', 'Lowercase');
 });
 
+//double checked with http://www.braingle.com/brainteasers/codes/keyword.php
 test('Mono substitution keyword', function(){
 	ok(mono.substitution("MONEY", mono.substitutionOptions.keywordEncrypt) == 'FHGTX', 'Encrypt');
 	ok(mono.substitution("FHGTX", mono.substitutionOptions.keywordDecrypt) == 'MONEY', 'Decrypt');
+	ok(mono.shift("00 7.", mono.substitutionOptions.keywordDecrypt) == '00 7.', 'unaltered');
+	ok(mono.substitution("small caps", mono.substitutionOptions.keywordEncrypt) == 'mfkee ykim', 'Lowercase encrypt');
+	ok(mono.substitution("mfkee ykim", mono.substitutionOptions.keywordDecrypt) == 'small caps', 'Lowercase decrypt');
 });
 
 
 test('Mono substitution affine', function(){
-	ok(mono.customFunction("AFFINECIPHERr", mono.customFunctionsOptions.affineEncrypt) == 'IHHWVCSWFRCP', 'Encrypt');
+	ok(mono.customFunction("AFFINECIPHER", mono.customFunctionsOptions.affineEncrypt) == 'IHHWVCSWFRCP', 'Encrypt');
+	ok(mono.customFunction("IHHWVCSWFRCP", mono.customFunctionsOptions.affineDecrypt) == 'AFFINECIPHER', 'Decrypt');
+	ok(mono.customFunction("affine small.", mono.customFunctionsOptions.affineEncrypt) == 'ihhwvc uqill.', 'Lowercase encrypt');
+	ok(mono.customFunction("ihhwvc uqill.", mono.customFunctionsOptions.affineDecrypt) == 'affine small.', 'Lowercase decrypt');
 });
+
+
+
+
 
